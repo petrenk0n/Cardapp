c9e440f84e4dbfdc68979a7958f65383b68f5985
Nikita Petrenko <thenikitapetrenko@gmail.com>
2021-01-16 17:53:12 -0500

    Fix truffle error

--- client/app.js
+++ client/app.js
@@ -44,17 +44,17 @@ App = {
     loadAccount: async () => {
       // Set the current blockchain account
       App.account = web3.eth.accounts[0]
-      console.log(App.account)
+      console.log("Hello")
     },
   
     loadContract: async () => {
       // Create a JavaScript version of the smart contract
-      const todoList = await $.getJSON('TodoList.json')
-      App.contracts.TodoList = TruffleContract(todoList)
-      App.contracts.TodoList.setProvider(App.web3Provider)
+      const carDetails = await $.getJSON('CarDetails.json')
+      App.contracts.CarDetails = TruffleContract(carDetails)
+      App.contracts.CarDetails.setProvider(App.web3Provider)
   
       // Hydrate the smart contract with values from the blockchain
-      App.todoList = await App.contracts.TodoList.deployed()
+      App.carDetails = await App.contracts.CarDetails.deployed()
     },
   
     render: async () => {
