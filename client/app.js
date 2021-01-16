cac9cbbcb348b88dc7a47d73c67f5379f150e014
Nikita Petrenko <thenikitapetrenko@gmail.com>
2021-01-16 18:25:54 -0500

    Fix metamask connection issue

--- client/app.js
+++ client/app.js
@@ -44,7 +44,7 @@ App = {
     loadAccount: async () => {
       // Set the current blockchain account
       App.account = web3.eth.accounts[0]
-      console.log("Hello")
+      console.log(App.account)
     },
   
     loadContract: async () => {
