7c4c84ebe469bfac9e22982e8776ea514ce99b2e
Nikita Petrenko <thenikitapetrenko@gmail.com>
2021-01-16 19:17:03 -0500

    Display car info on dashboard

--- client/app.js
+++ client/app.js
@@ -69,58 +69,49 @@ App = {
       // Render Account
       $('#account').html(App.account)
   
-      // Render Tasks
-      await App.renderTasks()
-  
       // Update loading state
       App.setLoading(false)
+
+      App.renderCars()
     },
-  
-    renderTasks: async () => {
-      // Load the total task count from the blockchain
-      const taskCount = await App.todoList.taskCount()
-      const $taskTemplate = $('.taskTemplate')
-  
-      // Render out each task with a new task template
-      for (var i = 1; i <= taskCount; i++) {
-        // Fetch the task data from the blockchain
-        const task = await App.todoList.tasks(i)
-        const taskId = task[0].toNumber()
-        const taskContent = task[1]
-        const taskCompleted = task[2]
-  
-        // Create the html for the task
-        const $newTaskTemplate = $taskTemplate.clone()
-        $newTaskTemplate.find('.content').html(taskContent)
-        $newTaskTemplate.find('input')
-                        .prop('name', taskId)
-                        .prop('checked', taskCompleted)
-                        .on('click', App.toggleCompleted)
-  
-        // Put the task in the correct list
-        if (taskCompleted) {
-          $('#completedTaskList').append($newTaskTemplate)
-        } else {
-          $('#taskList').append($newTaskTemplate)
+
+    // Render car info
+    renderCars: async () => {
+        const carCount = await App.carDetails.carCount()
+        const $carCard = $('.card-body')
+        
+        // Car representation
+        // id;
+        // carModel;
+        // vin;
+        // mileage;
+        // numOfOwners;
+        // numOfServiceVisits;
+        // numOfReportedAccidents;
+
+        // Retrieve all cars
+        for(var i = 1; i<= carCount; i++) {
+            const car = await App.carDetails.cars(i)
+            const carID = car[0].toNumber()
+            const carModel = car[1]
+            const carVin = car[2].toNumber()
+            const carMiles = car[3].toNumber()
+            const carOwners = car[4].toNumber()
+            const carServices = car[5].toNumber()
+            const carAccidents = car[6].toNumber()
+
+            $carCard.find('.car-model').html(carModel)  
+            $carCard.find('.car-id').html(carID)  
+            $carCard.find('.car-vin').html(carVin)  
+            $carCard.find('.car-miles').html(carMiles)  
+            $carCard.find('.car-owners').html(carOwners)  
+            $carCard.find('.car-services').html(carServices)  
+            $carCard.find('.car-accidents').html(carAccidents)
+
+            // const $newCarCard = $carCard.clone()
+            // $newCarCard.find('.car-model').html(carModel)
+            // console.log(carModel)
         }
-  
-        // Show the task
-        $newTaskTemplate.show()
-      }
-    },
-  
-    createTask: async () => {
-      App.setLoading(true)
-      const content = $('#newTask').val()
-      await App.todoList.createTask(content)
-      window.location.reload()
-    },
-  
-    toggleCompleted: async (e) => {
-      App.setLoading(true)
-      const taskId = e.target.name
-      await App.todoList.toggleCompleted(taskId)
-      window.location.reload()
     },
   
     setLoading: (boolean) => {
