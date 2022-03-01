0625d75329a16a808b786515554153fcb3e413c5
Michael Osmolovskiy <mosmolovskiy@gmail.com>
2022-03-01 08:23:25 -0500

    init next.js

--- client/script.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed(".typing", {
-    strings: ["Effortless.", "Secure.", "Honest."],
-    typeSpeed: 90, 
-    backSpeed: 60,
-    loop: true
-});
--- migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-const CarDetails = artifacts.require("./CarDetails.sol");
-
-module.exports = function (deployer) {
-  deployer.deploy(CarDetails);
-};
